457e28f0e1ae68d0e2785abc5ad028f1791ec6eb
gfremar <gfremar17@gmail.com>
2020-11-12 20:53:34 +0100

    OK 12/11/2020

--- PythonPractice/exercise9.py
+++ PythonPractice/exercise9.py
@@ -1,17 +1,25 @@
 import random
 intentos=0
-a=input("Introduce un número del 1 al 9. A ver si adivinas lo que he pensado. O escribe ""exit"" para salir")
-while a!="exit" or a not in (1,9):
-    a=input("Introduce un número del 1 al 9. A ver si adivinas lo que he pensado. O escribe ""exit"" para salir")
+aleatorio=random.randint(1,9)
+a=input("Introduce un número del 1 al 9. A ver si adivinas lo que he pensado. O escribe \"exit\" para salir: ")
+while a.lower()!="exit" and int(a) not in range(1,10):
+    a=input("Introduce un número del 1 al 9. A ver si adivinas lo que he pensado. O escribe \"exit\" para salir: ")
 if a.lower()=="exit":
     exit()
-aleatorio=random.randint(1,9)
-if a==aleatorio:
-    intentos+=1
-    print("Muy bien, acertaste el número. Solo has necesitado " + str(intentos) + " intento/s.")
-elif a<aleatorio:
-    print("Huyyyy... Casi... Tu número es demasiado bajo. Prueba otra vez")
-    intentos+=1
-else:
-    print("Huyyyy... Casi... Tu número es demasiado alto. Prueba otra vez")
-    intentos+=1
+while True:
+    if int(a)==aleatorio:
+        intentos+=1
+        print("Muy bien, acertaste el número. Solo has necesitado " + str(intentos) + " intento/s.")
+        intentos=0
+        aleatorio=random.randint(1,9)
+    elif int(a)<aleatorio:
+        print("Huyyyy... Casi... Tu número es demasiado bajo. Prueba otra vez")
+        intentos+=1
+    else:
+        print("Huyyyy... Casi... Tu número es demasiado alto. Prueba otra vez")
+        intentos+=1
+    a=input("Introduce un número del 1 al 9. A ver si adivinas lo que he pensado. O escribe \"exit\" para salir: ")
+    while a.lower()!="exit" and int(a) not in range(1,10):
+        a=input("Introduce un número del 1 al 9. A ver si adivinas lo que he pensado. O escribe \"exit\" para salir: ")
+    if a.lower()=="exit":
+        exit()
